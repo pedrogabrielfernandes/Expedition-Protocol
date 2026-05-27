94c70fef676e93adb24ee7e0c200c55ef3517557
Matheus <matheusmontergames@gmail.com>
2026-05-26 22:20:15 -0300

    Alteracoes feitas por matheus, sprite de ataque, zumbi e background

--- main.c
+++ main.c
@@ -40,11 +40,11 @@
 #define ATTACK_DRAW_W DRAW_W
 #define ATTACK_DRAW_H DRAW_H
 
-#define ZUMBI_DRAW_W 180
-#define ZUMBI_DRAW_H 180
+#define ZUMBI_DRAW_W 120
+#define ZUMBI_DRAW_H 120
 
 #define ZUMBI_OFFSET_X 50
-#define ZUMBI_OFFSET_Y 169
+#define ZUMBI_OFFSET_Y 110
 
 unsigned char colisao[ALTURA][3000];
 
@@ -379,7 +379,589 @@ int main(void) {
 
   al_start_timer(timer);
 
+<<<<<<< Updated upstream
   OpcaoMenu escolha = executar_menu(queue, timer, bg_menu, fonte);
+=======
+    OpcaoMenu escolha = executar_menu(queue, timer, bg_menu, fonte);
+
+    if (escolha == MENU_SAIR)
+    {
+        return 0;
+    }
+
+    al_flush_event_queue(queue);
+
+    ALLEGRO_BITMAP *bg = al_load_bitmap("assets/cenarios/background2.png");
+    ALLEGRO_BITMAP *mapa = al_load_bitmap("assets/cenarios/colisao2.png");
+    gerar_mapa_colisao(mapa);
+    ALLEGRO_BITMAP *idle = al_load_bitmap("assets/sprites/Samurai/Idle.png");
+    ALLEGRO_BITMAP *run = al_load_bitmap("assets/sprites/Samurai/Run.png");
+    ALLEGRO_BITMAP *jump = al_load_bitmap("assets/sprites/Samurai/Jump.png");
+    ALLEGRO_BITMAP *coracao = al_load_bitmap("assets/itens/vida.png");
+    ALLEGRO_BITMAP *zumbi_sprite = al_load_bitmap("assets/sprites/Zombies/Walk.png");
+    ALLEGRO_BITMAP *ataque1 = al_load_bitmap("assets/sprites/Samurai/Attack_1.png");
+    ALLEGRO_BITMAP *ataque2 = al_load_bitmap("assets/sprites/Samurai/Attack_2.png");
+    ALLEGRO_BITMAP *ataque3 = al_load_bitmap("assets/sprites/Samurai/Attack_3.png");
+
+    if (!bg || !mapa || !idle || !run || !jump ||
+        !coracao || !zumbi_sprite ||
+        !ataque1 || !ataque2 || !ataque3)
+    {
+        printf("Erro ao carregar bitmaps do jogo\n");
+        return 1;
+    }
+
+    Jogador jogador;
+    jogador.mov.x = 60;
+    jogador.mov.y = 253;
+    jogador.mov.vel_y = 0;
+    jogador.frame = 0;
+    jogador.no_chao = 0;
+    jogador.direcao = 0;
+    jogador.movendo = 0;
+    jogador.atacando = 0;
+    jogador.frame_ataque = 0;
+    jogador.tipo_ataque = 1;
+    jogador.ultimo_dano = 0;
+
+    Inimigo zumbi;
+    zumbi.x = 1200;
+    zumbi.y = 760;
+    zumbi.velocidade = 1.35f;
+    zumbi.direcao = 0;
+    zumbi.vivo = 1;
+    zumbi.frame = 0;
+    zumbi.vida = 100;
+
+    Temporizador tempo;
+    tempo.inicio = al_get_time();
+    tempo.atual = 0;
+    tempo.fim = 0;
+    tempo.ativo = 1;
+
+    tempo.quantidade_scores = 0;
+    carregar_ranking(&tempo);
+
+    char mensagem_final[100] = "Fase Concluída!";
+
+    VidaStatus *vetor_vidas = (VidaStatus *)malloc(MAX_VIDAS * sizeof(VidaStatus));
+    for (int i = 0; i < MAX_VIDAS; i++)
+    {
+        vetor_vidas[i].ativa = 1;
+        strcpy(vetor_vidas[i].status, "Inteira");
+    }
+
+    int linhas_matriz = ALTURA / 100 + 1;
+    int colunas_matriz = LARGURA / 100 + 1;
+    int **matriz_decorativa = criar_matriz_decorativa(linhas_matriz, colunas_matriz);
+
+    int rodando = 1;
+    ALLEGRO_EVENT ev;
+    ALLEGRO_KEYBOARD_STATE state;
+
+    while (rodando)
+    {
+        al_wait_for_event(queue, &ev);
+
+        if (ev.type == ALLEGRO_EVENT_DISPLAY_CLOSE)
+            rodando = 0;
+
+        if (ev.type == ALLEGRO_EVENT_TIMER)
+        {
+            jogador.frame += 0.15f;
+            jogador.movendo = 0;
+
+            al_get_keyboard_state(&state);
+
+            static int key_r_anterior = 0;
+
+            int key_r_atual =
+                al_key_down(&state, ALLEGRO_KEY_R);
+
+            if (key_r_atual && !key_r_anterior)
+            {
+                jogador.mov.x = 2500;
+            }
+            key_r_anterior = key_r_atual;
+
+            static int key_k_anterior = 0;
+
+            int key_k_atual =
+                al_key_down(&state, ALLEGRO_KEY_K);
+
+            if (key_k_atual && !key_k_anterior)
+            {
+                jogador.tipo_ataque++;
+
+                if (jogador.tipo_ataque > 3)
+                    jogador.tipo_ataque = 1;
+            }
+
+            key_k_anterior = key_k_atual;
+
+            static int key_j_anterior = 0;
+
+            int key_j_atual =
+                al_key_down(&state, ALLEGRO_KEY_J);
+
+            if (key_j_atual &&
+                !key_j_anterior &&
+                !jogador.atacando)
+            {
+                jogador.atacando = 1;
+                jogador.frame_ataque = 0;
+            }
+
+            key_j_anterior = key_j_atual;
+
+            if (tempo.ativo)
+                tempo.atual = al_get_time() - tempo.inicio;
+
+            jogador.no_chao = esta_no_chao(mapa, jogador.mov.x, jogador.mov.y);
+
+            // =========================
+            // MOVIMENTAÇÃO HORIZONTAL
+            // =========================
+
+            float novo_x = jogador.mov.x;
+
+            if (al_key_down(&state, ALLEGRO_KEY_D) && !jogador.atacando)
+            {
+                novo_x += VELOCIDADE;
+                jogador.direcao = 0;
+                jogador.movendo = 1;
+            }
+
+            if (al_key_down(&state, ALLEGRO_KEY_A) && !jogador.atacando)
+            {
+                novo_x -= VELOCIDADE;
+                jogador.direcao = ALLEGRO_FLIP_HORIZONTAL;
+                jogador.movendo = 1;
+            }
+
+            if (!colide_mapa(mapa, novo_x, jogador.mov.y))
+                jogador.mov.x = novo_x;
+
+            // =========================
+            // PULO
+            // =========================
+
+            if (al_key_down(&state, ALLEGRO_KEY_W) &&
+                jogador.no_chao &&
+                !jogador.atacando)
+            {
+                jogador.mov.vel_y = FORCA_PULO;
+            }
+
+            jogador.mov.vel_y += GRAVIDADE;
+
+            if (jogador.mov.vel_y > MAX_QUEDA)
+                jogador.mov.vel_y = MAX_QUEDA;
+
+            // =========================
+            // MOVIMENTAÇÃO VERTICAL
+            // =========================
+
+            float novo_y = jogador.mov.y + jogador.mov.vel_y;
+
+            if (!colide_mapa(mapa, jogador.mov.x, novo_y))
+            {
+                jogador.mov.y = novo_y;
+            }
+            else
+            {
+                jogador.mov.vel_y = 0;
+            }
+
+            jogador.no_chao = esta_no_chao(mapa, jogador.mov.x, jogador.mov.y);
+
+            // =========================
+            // QUEDA
+            // =========================
+
+            if (jogador.mov.y > ALTURA + 200)
+            {
+                perder_vida(vetor_vidas);
+
+                jogador.mov.x = 60;
+                jogador.mov.y = 253;
+                jogador.mov.vel_y = 0;
+            }
+
+            // =========================
+            // IA DO ZUMBI
+            // =========================
+
+            if (zumbi.vivo)
+            {
+                zumbi.frame += 0.08f;
+
+                if (jogador.mov.x < zumbi.x)
+                {
+                    zumbi.x -= zumbi.velocidade;
+                    zumbi.direcao = ALLEGRO_FLIP_HORIZONTAL;
+                }
+                else
+                {
+                    zumbi.x += zumbi.velocidade;
+                    zumbi.direcao = 0;
+                }
+            }
+
+            // =========================
+            //      HITBOX DE ATAQUE
+            // =========================
+
+            if (jogador.atacando &&
+                jogador.frame_ataque >= 2 &&
+                jogador.frame_ataque <= 2.5 &&
+                zumbi.vivo)
+            {
+                float atk_x;
+                float atk_y = jogador.mov.y;
+
+                if (jogador.direcao == 0)
+                    atk_x = jogador.mov.x + 40;
+                else
+                    atk_x = jogador.mov.x - 60;
+
+                float hitbox_zumbi_x = zumbi.x + 10;
+                float hitbox_zumbi_y = zumbi.y + 10;
+
+                float hitbox_zumbi_w = 45;
+                float hitbox_zumbi_h = 75;
+
+                float hitbox_ataque_w = 55;
+                float hitbox_ataque_h = 50;
+
+                if (
+                    atk_x < hitbox_zumbi_x + hitbox_zumbi_w &&
+                    atk_x + hitbox_ataque_w > hitbox_zumbi_x &&
+                    atk_y < hitbox_zumbi_y + hitbox_zumbi_h &&
+                    atk_y + hitbox_ataque_h > hitbox_zumbi_y)
+                {
+                    int dano = 0;
+
+                    if (jogador.tipo_ataque == 1)
+                        dano = 15;
+
+                    else if (jogador.tipo_ataque == 2)
+                        dano = 30;
+
+                    else
+                        dano = 50;
+
+                    zumbi.vida -= dano;
+
+                    if (zumbi.vida < 0)
+                        zumbi.vida = 0;
+
+                    if (zumbi.vida <= 0)
+                    {
+                        zumbi.vivo = 0;
+                    }
+                }
+            }
+
+            // =========================
+            // COLISÃO COM ZUMBI
+            // =========================
+
+            if (zumbi.vivo)
+            {
+                if (
+                    jogador.mov.x < zumbi.x + 60 &&
+                    jogador.mov.x + HITBOX_W > zumbi.x &&
+                    jogador.mov.y < zumbi.y + 80 &&
+                    jogador.mov.y + HITBOX_H > zumbi.y)
+                {
+                    if (al_get_time() - jogador.ultimo_dano > 1.0)
+                    {
+                        perder_vida(vetor_vidas);
+
+                        jogador.ultimo_dano = al_get_time();
+
+                        if (jogador.direcao == 0)
+                            jogador.mov.x -= 120;
+                        else
+                            jogador.mov.x += 120;
+
+                        jogador.mov.vel_y = -8;
+                    }
+                }
+            }
+
+            // =========================
+            // FINAL DO MAPA
+            // =========================
+
+            if (jogador.mov.x > 2400 && tempo.ativo)
+            {
+                tempo.fim = al_get_time();
+                tempo.atual = tempo.fim - tempo.inicio;
+                tempo.ativo = 0;
+
+                if (tempo.quantidade_scores < 10)
+                {
+                    tempo.ranking[tempo.quantidade_scores] = tempo.atual;
+                    tempo.quantidade_scores++;
+                }
+                else
+                {
+                    ordenar_ranking(tempo.ranking, 10);
+                    if (tempo.atual < tempo.ranking[9])
+                    {
+                        tempo.ranking[9] = tempo.atual;
+                    }
+                }
+                ordenar_ranking(tempo.ranking, tempo.quantidade_scores);
+                salvar_ranking(&tempo);
+            }
+
+            float draw_x = jogador.mov.x - HITBOX_OFFSET_X;
+            float draw_y = jogador.mov.y - HITBOX_OFFSET_Y;
+
+            al_clear_to_color(al_map_rgb(255, 255, 255));
+
+            al_draw_bitmap(bg, 0, 0, 0);
+
+            desenhar_matriz_fundo(
+                matriz_decorativa,
+                linhas_matriz,
+                colunas_matriz);
+
+            // =========================
+            // DESENHO DO ZUMBI
+            // =========================
+
+            float zumbi_draw_x = zumbi.x - ZUMBI_OFFSET_X;
+            float zumbi_draw_y = zumbi.y - ZUMBI_OFFSET_Y;
+
+            if (zumbi.vivo)
+            {
+                int frame_zumbi = (int)zumbi.frame % FRAMES_ZUMBI;
+
+                al_draw_scaled_bitmap(
+                    zumbi_sprite,
+
+                    frame_zumbi * 96,
+                    0,
+
+                    96,
+                    96,
+
+                    zumbi_draw_x,
+                    zumbi_draw_y,
+
+                    ZUMBI_DRAW_W,
+                    ZUMBI_DRAW_H,
+
+                    zumbi.direcao);
+            }
+            float barra_largura = 58;
+
+            float barra_x =
+                zumbi_draw_x + (ZUMBI_DRAW_W / 2) - (barra_largura / 2) - 2;
+
+            float barra_y = zumbi_draw_y + 26;
+
+            al_draw_filled_rectangle(
+                barra_x,
+                barra_y,
+
+                barra_x + barra_largura,
+                barra_y + 6,
+
+                al_map_rgb(80, 0, 0));
+
+            al_draw_filled_rectangle(
+                barra_x,
+                barra_y,
+
+                barra_x + (zumbi.vida * 0.6),
+                barra_y + 6,
+
+                al_map_rgb(255, 0, 0));
+
+            if (jogador.atacando)
+            {
+                int max_frames = 0;
+
+                if (jogador.tipo_ataque == 1)
+                    max_frames = 6;
+
+                else if (jogador.tipo_ataque == 2)
+                    max_frames = 4;
+
+                else
+                    max_frames = 3;
+
+                int f = (int)jogador.frame_ataque;
+
+                if (f >= max_frames)
+                    f = max_frames - 1;
+
+                ALLEGRO_BITMAP *atk = NULL;
+
+                if (jogador.tipo_ataque == 1)
+                    atk = ataque1;
+
+                else if (jogador.tipo_ataque == 2)
+                    atk = ataque2;
+
+                else
+                    atk = ataque3;
+
+                float atk_draw_x = draw_x - 10;
+                float atk_draw_y = draw_y - 1;
+
+                al_draw_scaled_bitmap(
+                    atk,
+
+                    128 * f,
+                    0,
+
+                    128,
+                    128,
+
+                    atk_draw_x,
+                    atk_draw_y,
+
+                    ATTACK_DRAW_W,
+                    ATTACK_DRAW_H,
+
+                    jogador.direcao);
+
+                jogador.frame_ataque += 0.30f;
+
+                if (jogador.frame_ataque >= max_frames)
+                {
+                    jogador.atacando = 0;
+                    jogador.frame_ataque = 0;
+                }
+            }
+            else if (!jogador.no_chao)
+            {
+                int f = (int)jogador.frame % FRAMES_JUMP;
+
+                al_draw_scaled_bitmap(
+                    jump,
+                    128 * f, 0,
+                    128, 128,
+                    draw_x,
+                    draw_y,
+                    DRAW_W,
+                    DRAW_H,
+                    jogador.direcao);
+            }
+            else if (jogador.movendo)
+            {
+                int f = (int)jogador.frame % FRAMES_RUN;
+
+                al_draw_scaled_bitmap(
+                    run,
+                    128 * f, 0,
+                    128, 128,
+                    draw_x,
+                    draw_y,
+                    DRAW_W,
+                    DRAW_H,
+                    jogador.direcao);
+            }
+            else
+            {
+                int f = (int)jogador.frame % FRAMES_IDLE;
+
+                al_draw_scaled_bitmap(
+                    idle,
+                    128 * f, 0,
+                    128, 128,
+                    draw_x,
+                    draw_y,
+                    DRAW_W,
+                    DRAW_H,
+                    jogador.direcao);
+            }
+
+            // =========================
+            // HUD
+            // =========================
+
+            char texto[50];
+
+            sprintf(texto, "Tempo: %.2f s", tempo.atual);
+
+            al_draw_filled_rectangle(
+                10, 10,
+                340, 70,
+                al_map_rgb(255, 255, 255));
+
+            al_draw_text(
+                fonte,
+                al_map_rgb(255, 215, 0),
+                1850,
+                0,
+                ALLEGRO_ALIGN_RIGHT,
+                "RANKING");
+
+            for (int i = 0; i < tempo.quantidade_scores; i++)
+            {
+                char texto_rank[50];
+                sprintf(texto_rank, "%d. %.2f s", i + 1, tempo.ranking[i]);
+                al_draw_text(
+                    fonte,
+                    al_map_rgb(255, 255, 255),
+                    1850,
+                    40 + i * 45,
+                    ALLEGRO_ALIGN_RIGHT,
+                    texto_rank);
+            }
+
+            al_draw_text(
+                fonte,
+                al_map_rgb(0, 0, 0),
+                20, 20,
+                0,
+                texto);
+
+            desenhar_vidas(vetor_vidas, coracao);
+
+            if (!tempo.ativo)
+            {
+                al_draw_text(
+                    fonte,
+                    al_map_rgb(255, 215, 0),
+                    LARGURA / 2.0,
+                    ALTURA / 3.0,
+                    ALLEGRO_ALIGN_CENTER,
+                    mensagem_final);
+            }
+
+            al_flip_display();
+        }
+    }
+
+    free(vetor_vidas);
+    liberar_matriz(matriz_decorativa, linhas_matriz);
+
+    al_destroy_bitmap(bg_menu);
+    al_destroy_bitmap(bg);
+    al_destroy_bitmap(mapa);
+    al_destroy_bitmap(idle);
+    al_destroy_bitmap(run);
+    al_destroy_bitmap(jump);
+    al_destroy_bitmap(coracao);
+
+    al_destroy_font(fonte);
+    al_destroy_timer(timer);
+    al_destroy_event_queue(queue);
+    al_destroy_display(display);
+    al_destroy_bitmap(zumbi_sprite);
+
+    al_destroy_bitmap(ataque1);
+    al_destroy_bitmap(ataque2);
+    al_destroy_bitmap(ataque3);
+>>>>>>> Stashed changes
 
   if (escolha == MENU_SAIR) {
     return 0;
